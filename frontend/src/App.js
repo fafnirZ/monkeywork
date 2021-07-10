0cb03f32f01cd312c17bbf2e044e4c61ad5f35b1
Jacky Xie <gokurohono@gmail.com>
2021-07-10 22:17:48 +1000

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
+import React from 'react';
 import './App.css';
 import { Nav } from './components/Nav/nav';
-import { SearchBar } from './components/SearchBar/searchBar'
+import { SearchBar } from './components/SearchBar/searchBar';
+import { Personalise } from './components/Personalise/personalise';
 
 function App() {
+  const [view, setView] = React.useState('search')
   return (
     <div className="App">
       <header className="App-header">
-        <Nav>hi</Nav>
+        <Nav/>
       </header>
       <body className="App-container">
-        <SearchBar/>
+        {(view === "search") && <SearchBar/>}
+        {(view === "personalise") && <Personalise/>}
       </body>
     </div>
   );
---
+++ frontend/src/components/Personalise/personalise.js
@@ -0,0 +1,8 @@
+import './personalise.css';
+export function Personalise(props) {
+    return (
+        <div className="personalise-container">
+            hi
+        </div>
+    )
+}
