ba6db6dfc037ecc7a96454490a9df6237944cee7
Willam <z5255056@cse.unsw.edu.au>
2021-07-14 22:59:56 +1000

    Searchbar functionality with popup

--- frontend/src/components/SearchBar/searchBar.js
+++ frontend/src/components/SearchBar/searchBar.js
@@ -1,10 +1,56 @@
 import './searchBar.css';
+import React, { onClick, useState } from 'react';
+import Popup from './popup.js';
 
 export function SearchBar(props) {
 
+    const [popUp, setPopUp] = useState(false);
+    const closePopUp = () => setPopUp(false);
+    const openPopUp = () => setPopUp(true);
+
+    const [search, setSearch] = useState('');
+
+    const [disable, setDisable] = useState(true);
+    function handleDisable(search) {
+        setDisable(search.target.value === '');
+    }
+
     return (
-        <div className="Search-container">
-            searchbar placeholder
+        <div>
+            <div className="Search-container">
+                <div className="Search-prompt">
+                    I want...
+                </div>
+
+                <div className="testbox">
+                    <input                     
+                        className="Search-bar"
+                        type="text"
+                        id="search"
+                        placeholder="What do you want?"
+                        onChange={handleDisable}
+                    />
+                    <div>
+                        <button 
+                            className="Search-button-camera"
+                            onClick={() => {setPopUp(true)}}
+                            disabled={disable}
+                        ></button>
+                    </div>
+                    <div>
+                        <button 
+                            onClick={() => {setPopUp(true)}}
+                            disabled={disable}
+                        > 
+                            <img src='Circle-arrow.svg'/>
+                        </button>                    
+                    </div>
+                </div>
+
+            </div>
+            <div>
+                <Popup search={search} popUp={popUp} closePopUp={closePopUp}/>
+            </div>
         </div>
     );
 }
