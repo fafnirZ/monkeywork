26c3a1a2c052b006992498fdf061caa0725212c5
Jacky Xie <gokurohono@gmail.com>
2021-07-15 14:55:58 +1000

    edit works to a good extent

--- frontend/src/components/Personalise/personalise.js
+++ frontend/src/components/Personalise/personalise.js
@@ -3,7 +3,7 @@ import Tab from '../Tabs/Tab';
 
 import React, {useState} from 'react';
 import { react } from '@babel/types';
-import { Checkbox } from './checkbox';
+// import { Checkbox } from './checkbox';
 import { Delete } from './delete';
 
 
@@ -12,10 +12,30 @@ export function Personalise(props) {
     // {"flour": {"value": "1 cup", "checked": false},"sugar": {"value": "300g", "checked": true},"butter": {"value": "500g", "checked": false}}
     const [items, setItems] = useState({});
     const [view, setView] = useState('myList');
+    const [force, updateState] = React.useState();
+    const forceUpdate = React.useCallback(() => updateState({}), []);
 
-    const handleSubmit = (keyy, newValue) => {
+    const handleSubmit = (prevState, newState) => {
         try {
-            console.log(newValue);
+            const {prevKey, prevValue}  = prevState
+            const {newKey, newValue}  = newState
+            // now change the state in localstorage
+
+            let ingredients = JSON.parse(localStorage['ingredients']);
+            if(prevKey !== newKey) {
+                // initialise checked as false again
+            
+                ingredients[newKey] = {'value': newValue}
+                delete ingredients[prevKey]
+                localStorage.setItem('ingredients', JSON.stringify(ingredients))
+
+
+                //console.log(newObject)
+                setItems(ingredients)
+                
+                console.log(ingredients)
+            }
+
         } catch(err) {
             console.log(err);
         }
@@ -32,7 +52,7 @@ export function Personalise(props) {
             console.log('there is no ingredients')
         }
 
-    },[])
+    },[force])
 
 
 
@@ -46,7 +66,7 @@ export function Personalise(props) {
                     console.log(`key: ${key}`)
                     console.log(`value ${value}`)
                     return (
-                        <Items keyy={key} value={value} checkBox={true} handleSubmit={handleSubmit}/>
+                        <Items keyy={key} value={value} checkBox={true} handleSubmit={handleSubmit} forceUpdate={forceUpdate}/>
                     )
                 })}       
             </div>
@@ -68,17 +88,38 @@ im just going to export this
 export function Items(props) {
     const [editable, setEditable] = React.useState(false);
     const itemRef = React.useRef();
-    const inputRef = React.useRef();
+    const valueRef = React.useRef();
+    const keyRef = React.useRef();
+    const [displayValue, setDisplayValue] = React.useState('');
 
     const toggleEdit = (e) => {
         if(editable) {
-            const newValue = inputRef.current.data
-            const {data, value} = inputRef.current
+            const newValue = valueRef.current.value
+            const newKey = keyRef.current.value
+
+            const prevValue = valueRef.current.defaultValue
+            const prevKey = keyRef.current.defaultValue
+
+            props.handleSubmit({prevKey, prevValue},{newKey, newValue})
+
+            //previous state is <input defaultvalue>
+
+            /*
             console.log(value);
-            props.handleSubmit(props.keyy, newValue);
+            props.handleSubmit(props.keyy, newValues);
+            */
+          
         }
         setEditable(!editable);
     }
+
+    React.useEffect(() => {
+        if(props.keyy !== undefined && props.value !== undefined) {
+            const display = props.value.value + ' ' + props.keyy
+            console.log(display)
+            setDisplayValue(display)
+        }
+    },[props])
 
 
     return (
@@ -86,18 +127,19 @@ export function Items(props) {
         <div className="checklist-item-container" style={props.style} checkBox={props.checkBox}
             contentEditable={props.contentEditable}
             ref={itemRef}
-           
             >
-            {(props.checkBox) && <Checkbox ingredient = {props.keyy}/>}
-            {editable && <input ref={inputRef}
-                defaultValue={props.value.value+' '+props.keyy}
-                
-            />}
+            {/*(props.checkBox) && <Checkbox ingredient = {props.keyy}/>*/}
+            {editable && 
+            <>
+            <input ref={valueRef} defaultValue={props.value.value} style={{'width': '30%'}}/>
+            <input ref={keyRef} defaultValue={props.keyy} style={{'width':'40%'}}/>                
+            </>
+            }
             {!editable && 
-                <>
-                <p>{props.value.value}</p>
-                <p>{props.keyy}</p>
-                </>
+                <div>
+                    {displayValue}
+                </div>
+                
             }
             <div className="edit-button" onClick={toggleEdit}>
                 <img src="edit.svg"/>
